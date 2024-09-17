de8c0f6c57e1d9b3367072472eb408c981a7cea5
SempaiDarcy <mr_darcy_1@mail.ru>
2024-09-18 04:23:56 +0500

    feat: init basic tkinter app structure

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-# Определение состояний системы
-S1 = "Полностью работоспособна"
-S2 = "Работает с ограничениями"
-S3 = "В состоянии отказа"
+import tkinter as tk
 
+class MarkovApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Моделирование Марковского процесса")
+        self.root.geometry("700x600")  # Размер окна приложения
+
+# Основная программа для запуска приложения
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = MarkovApp(root)
+    root.mainloop()
